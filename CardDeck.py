baea5039946ac45e09ef5dd309d232f9ce446aaf
Ausra Grigaliune <ausra.grigaliune@gmail.com>
2024-03-12 13:11:35 +0200

    Start from zero

---
+++ CardDeck.py
@@ -0,0 +1,13 @@
+class Cards:
+    def __init__(self):   
+
+    # def card_deck(self):
+    #     create
+    #     shuffle
+        
+
+    # def card_split(self, number of players?):
+    #     player1
+    # ````player2
+
+        pass
