4f832724f360bb0a43127e0bdd85a2cee37cccc9
OKDavies <110639746+OKDavies@users.noreply.github.com>
2024-03-13 15:20:55 +0000

    Card class restructured

--- CardDeck.py
+++ CardDeck.py
@@ -1,3 +1,14 @@
+from random import shuffle
+
+class Card:
+    def __init__(self, sign, rank, value) -> None:
+        self.sign = sign
+        self.rank = rank
+        self.value = value
+
+    def __str__(self) -> str:
+        return f"{self.sign}{self.rank}"
+
 
 class CardDeck:
     def __init__(self): 
@@ -5,27 +16,30 @@ class CardDeck:
     
     def card_deck(self):
         signs = ["\u2664", "\u2661", "\u2662", "\u2667"]
-        card_name = ["A", "K", "Q", "J", "10", "9", "8", "7", "6", "5", "4", "3", "2"]
+        card_rank_value = {"A": 14, "K": 13, "Q": 12, "J": 11, 
+                           "10": 10, "9": 9, "8": 8, "7": 7,
+                            "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
         card_deck = []
         for sign in signs:
-            for name in card_name:
-                card = f"{name}{sign}"
-                card_deck.append(card)
+            for rank in card_rank_value:
+                card_deck.append(Card(sign, rank, card_rank_value[rank]))  
         return card_deck
 
     def __str__(self):
-        return str(self.cardDeck)
+        """Only testing purpose, will return error"""
+        for card in self.cardDeck:
+            print(card)
     
     def deck_shuffle(self):
-        pass
+        shuffle(self.cardDeck)
 
     def card_split(self, number_of_players):
         # return deck1, deck2...
         pass
-       
+ 
     def card_compare(self, card1, card2):
         pass
 
 
-c = Cards()
-print(c)
+#c = Cards()
+#print(c)
--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import CardDeck
+
+class Player:
+    def __init__(self, name, hand) -> None:
+        self.name = name
+        self.hand = hand
 
 
 def main():
+    c = CardDeck()
+    c.card_deck()
+    c.deck_shuffle()
+    p1_hand, p2_hand = c.card_split()
+    name = input("Enter player 1 name: ")
+    player1 = Player(name, p1_hand)
+    name = input("Enter player 2 name: ")
+    player2 = Player(name, p2_hand)
     print("""
     Welcome to the War card game!
     """)
