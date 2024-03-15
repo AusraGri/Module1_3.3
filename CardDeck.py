a5a730434dd5ae3179e4cd5c60002eb5ef5697c2
AusraGri <157502081+AusraGri@users.noreply.github.com>
2024-03-15 20:26:17 +0200

    lay cards function added

--- CardDeck.py
+++ CardDeck.py
@@ -13,7 +13,6 @@ class Card:
 class CardDeck(Card):
     def __init__(self):  
         self.cardDeck = self.card_deck() 
-        self.player_hands = ""
     
     def card_deck(self):
         signs = ["\u2664", "\u2661", "\u2662", "\u2667"]
@@ -25,14 +24,14 @@ class CardDeck(Card):
             for rank in card_rank_value:
                 card_deck.append(Card(sign, rank, card_rank_value[rank]))  
         return card_deck
-
-    def __str__(self):
-        return self.player_hands
     
     def deck_shuffle(self):
         shuffle(self.cardDeck)
 
-    def card_split(self, number_of_players):
+    def __str__(self) -> str:
+        return super().__str__()
+
+    def card_split(self, number_of_players=2):
         if int(number_of_players)>0 and int(number_of_players)%2 == 0:
             players = int(number_of_players)
             hand_size = len(self.cardDeck) // players
@@ -41,28 +40,14 @@ class CardDeck(Card):
             for _ in range(players):
                 hand_end = start + hand_size 
                 hands.append(self.cardDeck[start:hand_end])
-                start = hand_end + 1
-            # self.player_hands = hands
-            return hands
+                start = hand_end
+            return hands[0], hands[1]
         else:
             print("Error: Wrong number as parameter")
-
-    
- 
-    def card_compare(self, card1, card2):
-        pass
+        
 
 
-c = CardDeck()
-hands = c.card_split(4)
-player1 = hands[0]
-player2 = hands[1]
-print(len(player1))
-print(len(player2))
-
-# print(hands)
-# for i, hand in enumerate(hands, start=1):
-#     print(f"Player {i} cards:")
-#     for card in hand:
-#         print(card)
-#     print() 
+# c = CardDeck()
+# hand1, hand2 = c.card_split()
+# print(len(hand1))
+# print(len(hand2))
--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-import CardDeck
+from CardDeck import CardDeck
 
 class Player:
     def __init__(self, name, hand) -> None:
         self.name = name
         self.hand = hand
+
+    def lay_card(self):
+        card = self.hand[0]
+        self.hand.remove(self.hand[0])
+        return card
+    
+    def take_card(self, cards):
+        for card in cards:
+            self.hand.append(card)
 
 
 def main():
@@ -11,13 +20,25 @@ def main():
     c.card_deck()
     c.deck_shuffle()
     p1_hand, p2_hand = c.card_split()
-    name = input("Enter player 1 name: ")
+    name = "DAVE"
     player1 = Player(name, p1_hand)
-    name = input("Enter player 2 name: ")
-    player2 = Player(name, p2_hand)
-    print("""
-    Welcome to the War card game!
-    """)
+    name2 = "BOB"
+    player2 = Player(name2, p2_hand)
+    card1 = player1.lay_card()
+    card2 = player2.lay_card()
+    compare_cards(card1.value, card2.value)
+
+    
+    
+def compare_cards(card1, card2):
+    if card1 > card2:
+        print(f"{card1} higher than {card2}")
+    elif card2 > card1:
+        print(f"{card2} higher than {card1}")
+    else:
+        print(f"{card1} equal {card2}")
+
+
 
 
 if __name__=="__main__":
